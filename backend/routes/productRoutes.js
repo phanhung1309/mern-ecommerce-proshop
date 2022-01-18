314e8be308d7dd43cb3f1e400fe19cd48b967e3e
Hung Phan <phanhung1309@gmail.com>
2022-01-18 22:52:48 +0700

    Create admin delete product endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,13 @@
 import express from 'express'
 const router = express.Router()
-import { getProductById, getProducts } from '../controllers/productController'
+import {
+  getProductById,
+  getProducts,
+  deleteProduct,
+} from '../controllers/productController'
+import { protect, admin } from '../middleware/authMiddleware'
 
 router.route('/').get(getProducts)
-router.route('/:id').get(getProductById)
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
 
 export default router
