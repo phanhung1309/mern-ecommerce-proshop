b32eb35cb52703660fa79b23f45c089d0d069900
Hung Phan <phanhung1309@gmail.com>
2022-01-22 00:04:01 +0700

    Image upload config and endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
@@ -7,6 +8,7 @@ import connectDB from './config/db'
 import productRoutes from './routes/productRoutes'
 import userRoutes from './routes/userRoutes'
 import orderRoutes from './routes/orderRoutes'
+import uploadRoutes from './routes/uploadRoutes'
 
 dotenv.config()
 
@@ -23,10 +25,14 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID)
 })
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 
