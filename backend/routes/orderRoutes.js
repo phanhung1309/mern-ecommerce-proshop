bcd887fd04d7d12a7c159583d1db4fc642e6eeb3
Hung Phan <phanhung1309@gmail.com>
2022-01-26 11:39:05 +0700

    Admin order list

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,12 +3,13 @@ const router = express.Router()
 import {
   addOrderItems,
   getMyOrders,
+  getOrders,
   getOrderById,
   updateOrderToPaid,
 } from '../controllers/orderController'
-import { protect } from '../middleware/authMiddleware'
+import { protect, admin } from '../middleware/authMiddleware'
 
-router.route('/').post(protect, addOrderItems)
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders)
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
